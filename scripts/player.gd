2673e2c7957d1f7ea5e4b4a8b45161abe33a4c5a
wanderson <wanderson.alves.rodrigues@gmail.com>
2026-07-24 13:43:56 -0300

    feat: adição camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 30.0
+const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
